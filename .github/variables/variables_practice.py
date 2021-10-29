7d520b63feac73c9f7cd586ddc4032e64f2d604e
MurrieA <90346474+MurrieA@users.noreply.github.com>
2021-10-29 14:49:45 -0400

    version 0.2
    Printed() lives_remaining, forty_time, and death_cry.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,8 +1,12 @@
-# Variables Practice, Alyssa Murrie, 10/29/21 2:37, version 0.1
+# Variables Practice, Alyssa Murrie, 10/29/21 2:47, version 0.2
 
 # Three Common Types of Data
 
 lives_remaining = 13 # INTERGERS, any +/- WHOLE number and 0.
 forty_time = 4.21 # FLOATING POINT, and +/- number with a decimal.
-death_cry = "Oh, is that all you got?"
-# Line 7 is a STRING -- collection of letters/numbers/other chars. 
+death_cry = "This isn't over ZUES!"
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
+
+print(lives_remaining) 
+print(forty_time)
+print(death_cry) 
