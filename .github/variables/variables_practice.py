a28e63692acfcf1c41b7bad1e2c54fc2ff5ac329
MurrieA <90346474+MurrieA@users.noreply.github.com>
2021-10-29 14:40:29 -0400

    version 0.1
    Added string, int and Floating lines.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Alyssa Murrie, 10/29/21 2:27, version 0.0
+# Variables Practice, Alyssa Murrie, 10/29/21 2:37, version 0.1
+
+# Three Common Types of Data
+
+lives_remaining = 13 # INTERGERS, any +/- WHOLE number and 0.
+forty_time = 4.21 # FLOATING POINT, and +/- number with a decimal.
+death_cry = "Oh, is that all you got?"
+# Line 7 is a STRING -- collection of letters/numbers/other chars. 
